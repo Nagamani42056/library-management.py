84af969cf1fab92a26677147c986ea8fc1c46b27
Nagamani42056 <daggavolunagamani@gmail.com>
2026-06-25 12:17:17 +0530

    Update password generator.py
    # Password Generator
    
    ## Overview
    A Python-based Password Generator that creates strong and secure passwords using letters, numbers, and special characters.
    
    ## Features
    - Custom password length
    - Random password generation
    - Includes uppercase and lowercase letters
    - Includes digits and special characters
    
    ## Technologies Used
    - Python
    
    ## How to Run
    python password_generator.py

--- password generator.py
+++ password generator.py
@@ -1,16 +1,1 @@
-# Password Generator
 
-## Overview
-A Python-based Password Generator that creates strong and secure passwords using letters, numbers, and special characters.
-
-## Features
-- Custom password length
-- Random password generation
-- Includes uppercase and lowercase letters
-- Includes digits and special characters
-
-## Technologies Used
-- Python
-
-## How to Run
-python password_generator.py
