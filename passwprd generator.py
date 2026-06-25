e0425b557b0124ad9139a432f59afd36fae3c863
Nagamani42056 <daggavolunagamani@gmail.com>
2026-06-25 12:18:13 +0530

    Add files via upload

---
+++ passwprd generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+all_characters = letters + digits + symbols
+
+password = ''.join(random.choice(all_characters)
+                   for _ in range(length))
+
+print("\nGenerated Password:", password)
